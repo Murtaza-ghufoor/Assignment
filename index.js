e2efee624b47327c0470cbd7a727ebc8c4deb27b
murtaza-ghufoor <murtazaabdulghufoor@gmail.com>
2024-04-17 09:18:54 -0700

    hi this tsc practice code for let

---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict";
+// question no 1
+let myfavoratemoviename = '3 Idiots';
+console.log(myfavoratemoviename);
+// question 2
+let Name = 'malik aurangzaib';
+let age = '22';
+console.log(`aslam o alaikum my name is ${Name} and my age is ${age}`);
+// question no 3
+const currentmood = true;
+console.log(currentmood);
+// question no 4
+let firstname = 'Malik';
+let lastname = 'Aurangzaib';
+let fullname = `${firstname} ${lastname}`;
+console.log(fullname);
+// question no 5
+let userage = 'user age is 22';
+console.log(userage);
+// question no 6
+let productname = 'ABC';
+let productprice = '200000';
+console.log(`product: ${productname}, price: ${productprice}`);
+// question no 7
+let numberofpets = 10;
+console.log(numberofpets);
+// question no 8
+let isyourbirthdaytoday = false;
+console.log(isyourbirthdaytoday);
+// question no 9
+console.log(`first name: ${firstname}`);
+console.log(`last name: ${lastname}`);
+console.log(`age: ${age}`);
+//question no 10
+const birthyear = 2003;
+console.log(birthyear);
+// question no 11
+let city = 'Mana wala';
+let country = 'pakistan';
+let fulladdress = `${city} ${country}`;
+console.log(fulladdress);
+//question no 12
+let todaystemprature = 41;
+console.log(todaystemprature);
---
+++ index.ts
@@ -0,0 +1,77 @@
+// question no 1
+
+let myfavoratemoviename: string='3 Idiots';
+console.log(myfavoratemoviename);
+
+// question 2
+
+let Name: string='malik aurangzaib';
+let age: string='22';
+console.log(`aslam o alaikum my name is ${Name} and my age is ${age}`);
+
+// question no 3
+
+
+const currentmood: boolean = true;
+console.log(currentmood);
+
+// question no 4
+
+let firstname: string='Malik';
+let lastname: string='Aurangzaib';
+
+let fullname: string=`${firstname} ${lastname}`
+console.log(fullname);
+
+// question no 5
+
+let userage: string='user age is 22';
+console.log(userage);
+
+// question no 6
+
+let productname: string='ABC';
+let productprice: string='200000';
+
+console.log(`product: ${productname}, price: ${productprice}`)
+
+// question no 7
+
+let numberofpets: number =10;
+console.log(numberofpets);
+
+// question no 8
+
+let isyourbirthdaytoday: boolean=false;
+console.log(isyourbirthdaytoday);
+
+// question no 9
+
+console.log(`first name: ${firstname}`);
+console.log(`last name: ${lastname}`);
+console.log(`age: ${age}`);
+
+//question no 10
+
+const birthyear: number = 2003;
+console.log(birthyear);
+
+// question no 11
+
+let city: string='Mana wala';
+let country: string='pakistan';
+
+let fulladdress: string= `${city} ${country}`
+console.log(fulladdress);
+
+//question no 12
+
+let todaystemprature: number= 41;
+console.log(todaystemprature);
+
+
+
+
+
+
+
